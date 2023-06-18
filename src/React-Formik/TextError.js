5e4634961483ee3d9ac62cd6efba4613e6acade3
Mazharuddin-S <er.mazharuddin@gmail.com>
2023-06-18 21:33:08 +0530

    product component css changed

--- src/React-Formik/TextError.js
+++
@@ -1,3 +0,0 @@
-export const TextError = props => {
-  return <div className="Error">{props.children}</div>;
-};
--- src/Redux/Cake/IceCream/dist/IceCreamType.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.BUY_ICECREAM = void 0;
-var BUY_ICECREAM = "BUY_ICECREAM";
-exports.BUY_ICECREAM = BUY_ICECREAM;
